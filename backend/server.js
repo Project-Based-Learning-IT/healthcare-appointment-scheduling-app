232aefb0ea572fb4461a9931869e3d73428b3719
siddheshkothadi <realsiddheshko@gmail.com>
2021-03-07 11:55:32 +0530

    added template code

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening on port 5000`)
