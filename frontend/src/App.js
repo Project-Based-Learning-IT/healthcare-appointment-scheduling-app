b64b4bf3f5978523e0698e2e2232fa1cc16b13d0
Mayank8085 <mayanksahu1806@gmail.com>
2021-03-09 17:32:39 +0530

    add home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar.component";
+
 import Patient from "./components/patient.component";
 import DoctorLogin from "./components/doctor-login.component";
 import Doctor from "./components/doctor.component";
-import Homepage from "./components/homepage.component";
+import Navbar from './Home/Navbar'
+import Home from "./Home/Home";
 
 function App() {
   return (
+   
     <Router>
+       <Navbar/>
+       <Home/>
       <div className="container">
-        <Navbar />
+      
+       
         <br />
-        <Route path="/" exact component={Homepage} />
         <Route path="/patient/:id" component={Patient} />
         <Route path="/doctor/:id" component={Doctor} />
         <Route path="/doctor-login" component={DoctorLogin} />
---
+++ frontend/src/Home/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Image from '../image/doctor.jpg'
+
+const About=()=>{
+
+    return(
+        <div class="row m-3">
+  <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae deleniti, blanditiis est harum suscipit accusamus pariatur minus error aliquam quos porro quidem sit, minima nobis quisquam perspiciatis quia amet saepe!
+  Soluta itaque totam perferendis, quo voluptas porro cum? Nam nostrum quas aut! Amet et mollitia illo laborum facilis? Quidem provident expedita est inventore libero adipisci eaque qui quia hic. Eligendi.
+  Laboriosam atque ea fugit aut ratione excepturi repellat animi nobis magni enim numquam quisquam, iste voluptate laborum obcaecati eaque quis molestias incidunt odio, reiciendis ut facilis rem alias. Aut, modi.
+  Provident, corrupti architecto quibusdam enim pariatur harum earum cupiditate, magni voluptates deserunt minus eos ab libero quam id praesentium optio! Nemo aspernatur natus nisi. Id aliquid quia omnis molestias aperiam!
+  Iure beatae, fuga ratione voluptatem debitis consequatur facilis voluptas soluta, exercitationem voluptatum animi cupiditate atque excepturi pariatur! Ea sit, corrupti molestiae distinctio maxime repudiandae, consectetur illo, praesentium repellendus cumque provident?
+  Suscipit temporibus, praesentium voluptates aperiam nisi ut animi fuga blanditiis optio! Odio repudiandae, voluptatem, accusamus vel quae voluptatibus sequi, nemo fuga inventore omnis deserunt ad tempora quod doloribus eius asperiores?
+  Sapiente fuga architecto iure repudiandae ratione placeat earum cum odio, aspernatur odit, beatae repellendus! Nostrum ab incidunt ullam optio inventore, odit cum deleniti reiciendis repellendus porro veniam tempora nesciunt iste.
+  Recusandae esse optio sed vitae laboriosam amet! Quod maxime ducimus ratione adipisci ea esse cupiditate minus reiciendis cum architecto! Deserunt provident delectus qui quibusdam praesentium? Cupiditate eligendi fuga iure est?
+  Sunt accusamus facere distinctio, at mollitia, hic iste quae praesentium aliquam animi quisquam. Aliquid numquam necessitatibus asperiores nisi nostrum accusamus delectus quod ad praesentium facilis doloremque nulla voluptatem, fugiat aliquam.
+  Nobis provident minima tenetur eligendi vel laudantium debitis facere maxime repudiandae quis, at repellendus earum, dolore voluptate sunt consequatur labore sed adipisci eius est. A unde rerum beatae pariatur nemo?</div>
+  <div class="col-4">
+      <img src={Image}
+      width="100%"
+      height="100%"
+      
+      alt=""/>
+  </div>
+</div>
+    )
+
+}
+
+export default About;
