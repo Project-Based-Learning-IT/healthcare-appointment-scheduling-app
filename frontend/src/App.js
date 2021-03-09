01051389cf866311a00129765a40f44c4ce7ca91
Mayank8085 <mayanksahu1806@gmail.com>
2021-03-09 19:29:21 +0530

    modification in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,37 +3,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-<<<<<<< HEAD
-
-=======
 import Navbar from "./components/navbar.component";
 import Footer from "./components/footer.component";
->>>>>>> ce1bfb2876b343215ee99bdccf1fca0a00f0b937
 import Patient from "./components/patient.component";
 import Doctor_Login from "./components/doctor-login.component";
 import Doctor from "./components/doctor.component";
-import Navbar from './Home/Navbar'
+
+// import Navbar from './Home/Navbar'
 import Home from "./Home/Home";
+import Homepage from "./components/homepage.component";
 
 function App() {
   return (
-<<<<<<< HEAD
-   
-    <Router>
-       <Navbar/>
-       <Home/>
-      <div className="container">
-      
-       
-=======
     <div>
       <Router>
         <Navbar />
->>>>>>> ce1bfb2876b343215ee99bdccf1fca0a00f0b937
+        
         <br />
-        <Route path="/patient/:id" component={Patient} />
-        <Route path="/doctor/:id" component={Doctor} />
-        <Route path="/doctor-login" component={Doctor_Login} />
+
+        <Route exact path="/patient/:id" component={Patient} />
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/doctor/:id" component={Doctor} />
+        <Route exact path="/doctor-login" component={Doctor_Login} />
         <Footer />
       </Router>
     </div>
