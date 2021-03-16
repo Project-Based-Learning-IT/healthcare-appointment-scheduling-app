2e9bcb4e182d589299f4f6385126952bb1b9086e
Sidhant Khamankar <avatarmeher999@gmail.com>
2021-03-16 23:52:32 +0530

    two doctor cards in a row for searching

--- frontend/src/Doctor/Search.js
+++ frontend/src/Doctor/Search.js
@@ -2,58 +2,68 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import {
-    Row,
-    Col,
-    Input,
-    Button,
-    InputGroup,
-    InputGroupAddon
-  } from "reactstrap";
-  
+  Row,
+  Col,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon,
+} from "reactstrap";
 
 const Search = () => {
   const [Doctor, setDoctor] = useState([]);
 
   const fetchDoctor = async () => {
-    const { data } = await Axios.get(`${process.env.REACT_APP_SERVER_URL}/doctors/`);
+    const { data } = await Axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/doctors/`
+    );
     setDoctor(data);
-    console.log(data)
+    console.log(data);
   };
 
   useEffect(() => {
     fetchDoctor();
   }, []);
 
-
   return (
     <div>
-        <Row className="mb-3">
-            <Col>
+      <Row className="mb-3">
+        <Col>
           <InputGroup>
-            <Input
-              type="text"
-              placeholder="Search Your Doctor"
-            />
+            <Input type="text" placeholder="Search Your Doctor" />
             <InputGroupAddon addonType="append">
-              <Button  color="primary">
-                Search Doctor
-              </Button>
+              <Button color="primary">Search Doctor</Button>
             </InputGroupAddon>
-          </InputGroup> 
-        </Col>     
-        </Row>
-        <ListGroup>
-      
-      {Doctor.map(doc => (
-        <ListGroupItem key={doc.id} className="mb-3">
-          <div className="text-info"> <h6>Doctor Name:<span className="text-uppercase"> {doc.name}</span></h6> </div>
-          <div>Specialization : {doc.specialization}</div>
-          <div >Phone Number : {doc.phoneNumber}</div>
-          <div >FeesPerSession: {doc.feesPerSession}</div>
+          </InputGroup>
+        </Col>
+      </Row>
+      {/* <ListGroup> */}
+      <div className="overflow-auto ">
+        <div className="row">
+          {Doctor.map((doc) => (
+            // <ListGroupItem key={doc.id} className="mb-3">
+            <div className="col-sm-6 mb-2">
+              <div className="card">
+                <div className="card-body">
+                  <div className="text-info">
+                    {" "}
+                    <h6>
+                      Doctor Name:
+                      <span className="text-uppercase"> {doc.name}</span>
+                    </h6>{" "}
+                  </div>
+                  <div>Specialization : {doc.specialization}</div>
+                  <div>Phone Number : {doc.phoneNumber}</div>
+                  <div>FeesPerSession: {doc.feesPerSession}</div>
 
-        </ListGroupItem>
-      ))}
-    </ListGroup>
+                  {/* </ListGroupItem> */}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* </ListGroup> */}
     </div>
   );
 };
