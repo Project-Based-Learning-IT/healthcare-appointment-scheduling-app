3155711621cedfb27cfdef17c1e11097c00622ad
Mayank8085 <mayanksahu1806@gmail.com>
2021-03-16 17:07:27 +0530

    Added Search Doctor UI

---
+++ frontend/src/Doctor/Search.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import {
+    Row,
+    Col,
+    Input,
+    Button,
+    InputGroup,
+    InputGroupAddon
+  } from "reactstrap";
+  
+
+const Search = () => {
+  const [Doctor, setDoctor] = useState([]);
+
+  const fetchDoctor = async () => {
+    const { data } = await Axios.get(`${process.env.REACT_APP_SERVER_URL}/doctors/`);
+    setDoctor(data);
+    console.log(data)
+  };
+
+  useEffect(() => {
+    fetchDoctor();
+  }, []);
+
+
+  return (
+    <div>
+        <Row className="mb-3">
+            <Col>
+          <InputGroup>
+            <Input
+              type="text"
+              placeholder="Search Your Doctor"
+            />
+            <InputGroupAddon addonType="append">
+              <Button  color="primary">
+                Search Doctor
+              </Button>
+            </InputGroupAddon>
+          </InputGroup> 
+        </Col>     
+        </Row>
+        <ListGroup>
+      
+      {Doctor.map(doc => (
+        <ListGroupItem key={doc.id} className="mb-3">
+          <div className="text-info"> <h6>Doctor Name:<span className="text-uppercase"> {doc.name}</span></h6> </div>
+          <div>Specialization : {doc.specialization}</div>
+          <div >Phone Number : {doc.phoneNumber}</div>
+          <div >FeesPerSession: {doc.feesPerSession}</div>
+
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+    </div>
+  );
+};
+
+export default Search;
--- frontend/src/Patient/SearchDoctor.js
+++ frontend/src/Patient/SearchDoctor.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from '../Basic/Navbar';
 import LeftsidePatient from '../Dashbaord/LeftsidePatient';
+import Search from '../Doctor/Search';
+
 
 const SearchDoctor=()=>{
-    
+     
     return(
         <div className="bg-dark" style = {{height:"100vh"}}>
         <Navbar/>
@@ -13,7 +15,7 @@ const SearchDoctor=()=>{
     <div className="col-3 col-md-3 p-4 bg-white " style = {{height:"80vh"}} ><LeftsidePatient/></div>
     <div className="col-9 col-md-9 p-4"
      style={{ border: "15px solid yellow ",height:"80vh",backgroundColor:"#6c757d" }}> 
-       
+       <Search/>
       </div>
   </div>
       </div>
