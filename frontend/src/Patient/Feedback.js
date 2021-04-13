6a2d94976a8df5547cde2383c2172b61d9b38c38
Mayank8085 <mayanksahu1806@gmail.com>
2021-04-13 12:58:26 +0530

    Make Star and Ratting

--- frontend/src/Patient/Feedback.js
+++ frontend/src/Patient/Feedback.js
@@ -1,63 +1,101 @@
-import React from 'react';
-import Navbar from '../Basic/Navbar';
-import {Col,Row, Input, Button} from 'reactstrap'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import Navbar from "../Basic/Navbar";
+import { Col, Row, Input, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import { FaRegStar, FaStar } from "react-icons/fa";
 
-const Feedback=()=>{
+const Feedback = () => {
+  const [rating, setRating] = useState(0);
+useEffect(() => {
+ setRating(0)
+}, [])
 
-    return(<div>
-        <Navbar/>
-        <div class="container mt-5"
+  const GetRating = () => {
+    setRating(rating + 1);
+  };
+
+  const reduceRatting=()=>{
+    setRating(rating - 1);
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div
+        class="container mt-5"
         style={{
-            border: "15px solid #03203C ",
-            height: "65vh",
-            backgroundColor: "#35BDD0",
-          }}
-        >
-            <h4 className="text-center m-3">It will be helful if you share your Experience</h4>
-       <Row className="m-3">
-           
-           <Input
-           placeholder="Feel Free to Share your Experience"
-           />
-           
-       </Row>
-       <Row className="m-3">
-           
-           <Input
-           type="textarea"
-           placeholder="Provide Short Information"
-           maxLength="50px"
-           style={{height:"30vh"}}
-           />
+          border: "15px solid #03203C ",
+          height: "65vh",
+          backgroundColor: "#35BDD0",
+        }}
+      >
+        <h4 className="text-center m-3">
+          It will be helful if you share your Experience
+        </h4>
+        <Row className="m-3">
+          <Input placeholder="Feel Free to Share your Experience" />
+        </Row>
+        <Row className="m-3">
+          <Input
+            type="textarea"
+            placeholder="Provide Short Information"
+            maxLength="50px"
+            style={{ height: "25vh" }}
+          />
+        </Row>
+        <Row className="m-3">
+          <Col className="ml-4">
+            <h5>Rating</h5>
+          </Col>
+          <Col>
+            {rating >= 1 ? (
+              <FaStar className="ml-1" onClick={reduceRatting} />
+            ) : (
+              <FaRegStar className="ml-1" onClick={GetRating} />
+            )}
+            {rating >= 2 ? (
+              <FaStar className="ml-1" onClick={reduceRatting} />
+            ) : (
+              <FaRegStar className="ml-1" onClick={GetRating} />
+            )}
+            {rating >= 3 ? (
+              <FaStar className="ml-1" onClick={reduceRatting} />
+            ) : (
+              <FaRegStar className="ml-1" onClick={GetRating} />
+            )}
 
-           
-       </Row>
-       <div className="row  mt-5 ml-5">
-              <div className="col-4">
-                <Link to="/patient/previousappointments">
-                  <Button color="danger">GO BACK</Button>
-                </Link>
-              </div>
-              <div className="col-4 justify-content-end">
-                  
-              </div>
-             
-              <div className="col-4 justify-content-end ">
-                
-                  <Button color="warning">Sumbit</Button>
-        
-              </div>
-              </div>
-      
+            {rating >= 4 ? (
+              <FaStar className="ml-1" onClick={reduceRatting} />
+            ) : (
+              <FaRegStar className="ml-1" onClick={GetRating} />
+            )}
+            {rating >= 5 ? (
+              <FaStar className="ml-1" onClick={reduceRatting}  />
+            ) : (
+              <FaRegStar className="ml-1" onClick={GetRating} />
+            )}
 
-       
-  
-     </div>
-     
-        
-    </div>)
+            {console.log(rating)}
+          </Col>
+          <Col>
+            <h5>{rating}/5</h5>
+          </Col>
+        </Row>
+        <div className="row  mt-5 ml-5">
+          <div className="col-4">
+            <Link to="/patient/previousappointments">
+              <Button color="danger">GO BACK</Button>
+            </Link>
+          </div>
+          <div className="col-4 justify-content-end"></div>
 
-}
+          <div className="col-4 justify-content-end ">
+            <Button color="warning">Sumbit</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Feedback
+export default Feedback;
