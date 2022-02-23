9205062056cb0fd27d3ba0f60fc43113d9c23ff5
Mayank8085 <mayanksahu1806@gmail.com>
2022-02-23 20:28:13 +0530

    UI Updation 2

--- src/Basic/Footer.js
+++ src/Basic/Footer.js
@@ -1,37 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
- const Footer=()=> {
-   
-    return (
-      
-        
-          <div className=" row text-light bg-dark p-2" 
-          style={{maxWidth:"100%",margin:"auto"}}>
-         
-         <div className="col-12 col-md-6 w-100">
-         <h4 className="white-text">Contact Us</h4>
-                     <p className="grey-text">
-                         ABC 5078, Mumbai, India
-                     </p>
-                     <p className="grey-text">
-                        +919307531964
-                     </p>
-           
-         </div>
+const Footer = () => {
+  return (
+    <div className="footer bg-dark text-white p-3 container-fluid">
+      <div className="row">
+        <div className="col-md-4">
+          <address>
+            <strong>Address</strong>
+            <br />
+            <span>
+              <strong>
+                <a href=" https://www.google.com/maps/place/pune">
+                  Pune, Maharashtra, India
+                </a>
+              </strong>
+            </span>
+          </address>
+        </div>
+        <div className="col-md-4">
+          <p>
+            Teams:
+            <br />
+            <a href="https://www.mayanksahu.tech/" target="_blank">
+                Mayank Sahu
+            </a>
+            <br />
+            <a href="https://siddheshkothadi.vercel.app/" target="_blank">
+                Siddesh Kothari
+            </a>
+            <br />
+            <a href="https://www.linkedin.com/in/sidhant-khamankar/" target="_blank">
+                Siddhant Khamankar
+            </a>
+            <br />
+            <a href="https://www.linkedin.com/in/aaparikh/" target="_blank">
+                Atharva Parikh
+            </a>
+            <br />
 
-         <div className="col-12 col-md-6 w-100">
-         <h4 className="white-text">Social Media</h4>
-                     <ul>
-                         <li><Link to="#!"className="grey-text"> Facebook</Link></li>
-                         <li><Link to="#!"className="grey-text"> Instagram</Link></li>
-                         <li><Link to="#!"className="grey-text"> Twitter</Link></li>
-                     </ul>
-         </div>
-</div>
-          
-      
-    );
-  }
+          </p>
+        </div>
+        <div className="col-md-4">
+            <p>
+                Want to contribute to this project?
+                <br />
+                <a href="https://github.com/mayanksahu">
+                    <i className="fab fa-github"></i>
+                </a>
+
+            </p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- src/Home/Jumbo.js
+++ src/Home/Jumbo.js
@@ -1,70 +1,44 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import co1 from "../image/co1.jpg"
-import co2 from "../image/co2.jpg"
-import co4 from "../image/co4.jpg"
-// import co3 from "../image/co3.jpg"
-// import co5 from "../image/co5.jpg"
+import co1 from "../image/co1.png";
+import co2 from "../image/co2.png";
+import co3 from "../image/co3.png";
 
+const Jumbo = () => {
+  return (
+    <div className="my-5 container-fluid">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-75 mx-auto"
+            src={co1}
+            height={400}
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-75 mx-auto"
+            height={400}
+            src={co2}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-75 mx-auto"
+            height={400}
+            src={co3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
 
-
-const Jumbo=()=>{
-    return(
-        <div>
-            <div
-          style={{
-            width: "100%",
-            height:"100%",
-            top: 1,
-            position: "relative",
-            
-          }}
-        >
-          <div style={{width:"100%"}}>
-            <Carousel interval={2000} keyboard={false}>
-              <Carousel.Item style={{ height: "500px" }}>
-                <img
-                  alt = "carousel-image1"
-                  className="d-block w-100"
-                  
-                  src={
-                    co1
-                  }
-                />
-{/* 
-                <Carousel.Caption>
-                  <h3>First</h3>
-                </Carousel.Caption> */}
-              </Carousel.Item>
-              <Carousel.Item style={{ height: "500px" }}>
-                <img
-                  alt="carousel-image2"
-                  className="d-block w-100"
-                  src={
-                    co2
-                  }
-                />
-                {/* <Carousel.Caption>
-                  <h3>Second</h3>
-                </Carousel.Caption> */}
-              </Carousel.Item>
-              <Carousel.Item style={{ height: "500px" }}>
-                <img
-                  alt="carousel-image3"
-                  className="d-block w-100"
-                  src={
-co4                  }
-                />
-                {/* <Carousel.Caption>
-                  <h3>Third</h3>
-                </Carousel.Caption> */}
-              </Carousel.Item>
-            </Carousel>
-          </div>
-        </div>
-        </div>
-    )
-}
-
-
-export default Jumbo
+export default Jumbo;
